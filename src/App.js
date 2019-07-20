ca971102e9a664d962d7fa2b60e9f1d50ea0da6c
Mauro Insacco <mauro.insacco@gmail.com>
2019-07-20 14:44:26 +0200

    impletemented fire utils, firebase database and stuff

--- src/App.js
+++ src/App.js
@@ -17,12 +17,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      const userRef = await createUserProfileDocument(user);
-      userRef.onSnapshot(snapShot => {
-        console.log(snapShot);
-      });
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
